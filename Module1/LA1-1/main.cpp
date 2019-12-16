94b5b0f790523d17b0f6a9a83b7f15eadd791324
Nick <hudson.nick.p@gmail.com>
2019-12-16 10:17:08 -0700

    added min and max integer size

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -13,5 +13,12 @@ int main(){
     cin >> age; //cin for user input
 
     cout<< "I am "<< age << " years old."<<endl;
+
+    // get the size of the variable
+    cout<< "the size of an int is " << sizeof(int)<< " bytes on this system." <<endl;
+    cout<< "Min INteger value: " << INT64_MIN << "bytes. " << "Max INtegers value: "
+        << INT64_MAX << " bytes." << endl;
+
+
     return 0;
 }
