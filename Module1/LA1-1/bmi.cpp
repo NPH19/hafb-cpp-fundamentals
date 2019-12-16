32f6faa077e691fb91ed12a6d32cf66f0b4cf18d
Nick <hudson.nick.p@gmail.com>
2019-12-16 11:16:42 -0700

    added global variables

--- Module1/LA1-1/bmi.cpp
+++ Module1/LA1-1/bmi.cpp
@@ -8,6 +8,10 @@
 #include <iostream>
 using namespace std;
 
+const float kMeterToInches = 39.3701;
+const float kKgTolbs = 2.20462;
+const float kBMIimperial = 703;
+
 int main(){
     // Calculate BMI: weight(kg)/[height(m)]^2
     // Floating point variables: float, double
@@ -22,9 +26,16 @@ int main(){
     bmi = weight/(height*height);
 
     //cout<< "I weigh "<< weight <<" kg, and I am " << height << " meters tall."<<endl;
-    cout<< "Your bmi is: "<<bmi<<endl;
+    cout<< "Your bmi(metric) is: "<<bmi<<endl;
 
+    float impWeight, impHeight, impBMI;
 
+    impWeight = weight*kKgTolbs; // 1 kg = 2.204 lbs
+    impHeight = height*kMeterToInches; // 1 m = 39.37 inches
+
+    impBMI = impWeight*kBMIimperial/(impHeight*impHeight);
+
+    cout<< "Your bmi(metric) is: "<<impBMI<<endl;
 
     return 0;
 }
