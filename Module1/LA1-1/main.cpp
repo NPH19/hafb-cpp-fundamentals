af9abf0c441d34cee4f29c0510e6fef804cb1c9f
Nick <hudson.nick.p@gmail.com>
2019-12-16 09:49:09 -0700

    second commit

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main(){
+
+    int age;
+
     std::cout<< "I can code" <<std::endl;
+    cout<< "Another line under namespace std" << endl;
+    cout << "Wildcats"<<" "<<"WSU "<<"Ogden"<<endl;
     
+    cout << "Enter your age: ";
+    cin >> age; //cin for user input
+
+    cout<< "I am "<< age << " years old."<<endl;
     return 0;
 }
